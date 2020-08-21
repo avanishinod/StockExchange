485b537d93fc42c3399413e0fc845db8578019ae
Anurag <anuragsunilkumar1005@gmail.com>
2020-08-21 20:23:37 +0530

    first commit

---
+++ app/src/main/java/com/example/stocks/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.stocks;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view)
+    {
+        Log.i( "info","Button pressed");
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
