097e5f90f3a1bc1597375a2d62ab4551976d7a1f
Anurag <anuragsunilkumar1005@gmail.com>
2020-08-21 21:37:51 +0530

    first commit

--- app/src/main/java/com/example/stocks/MainActivity.java
+++ app/src/main/java/com/example/stocks/MainActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view)
     {
+        EditText buyTextEdit = (EditText) findViewById(R.id.buyTextEdit);
+        EditText sellEditText = (EditText) findViewById(R.id.sellEditText);
+        EditText quantityEditText = (EditText) findViewById(R.id.quantityEditText);
+
         Log.i( "info","Button pressed");
+
+        Log.i( "buy",buyTextEdit.getText().toString());
+        Log.i( "sell ", sellEditText.getText().toString());
+        Log.i("quantity",quantityEditText.getText().toString());
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
