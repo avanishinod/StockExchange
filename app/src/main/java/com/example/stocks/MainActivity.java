e76ec7451e9aa58922402f00483fcb21b7e021a0
Anurag <anuragsunilkumar1005@gmail.com>
2020-08-23 21:49:23 +0530

    Until Stamp Duty calculation

--- app/src/main/java/com/example/stocks/MainActivity.java
+++ app/src/main/java/com/example/stocks/MainActivity.java
@@ -3,16 +3,21 @@ package com.example.stocks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView BestSelling;
     EditText buytext, selltext, quantitytext;
     Button buttonsubmit;
+
+    private static DecimalFormat df = new DecimalFormat("0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +38,26 @@ public class MainActivity extends AppCompatActivity {
                 String sell = selltext.getText().toString();
                 String quantity = quantitytext.getText().toString();
 
-                Double buyDouble = Double.parseDouble(buy);
-                Double sellDouble = Double.parseDouble(sell);
-                Double quantityDouble = Double.parseDouble(quantity);
+                double buyDouble = Double.parseDouble(buy);
+                double sellDouble = Double.parseDouble(sell);
+                double quantityDouble = Double.parseDouble(quantity);
 
-                Double BestSP = (buyDouble*(6.05/100))+buyDouble;
+                //calculation part
+
+                double TurnOver = (buyDouble*quantityDouble)+(sellDouble*quantityDouble);
+                double BrokerageNSE = 0.0;
+                double STTTotalNSE = Math.round(TurnOver *0.001);
+               double ExchangeTXNCharge = Math.round(((0.00325/100)*TurnOver)*100.0)/100.0;
+               double ClearingCharge = Math.round(((0.0002/100)*TurnOver)*100.0)/100.0;
+               double GST = Math.round((0.18*(BrokerageNSE+ExchangeTXNCharge))*100.0)/100.0;
+               double SEBICharges = Math.round(((0.0001/100)*TurnOver)*100.0)/100.0;
+               double StampDuty = Math.round((0.00015*(buyDouble*quantityDouble))*100.0)/100.0;
+               double TotalTaxAndCharges = BrokerageNSE+STTTotalNSE+ClearingCharge+GST+SEBICharges+StampDuty;
+
+
+               // Display Part
+
+                double BestSP = (buyDouble*(6.05/100))+buyDouble;
                 String BestSPString = Double.toString(BestSP);
 
                 BestSelling.setText("Best Selling Price "+ BestSPString);
